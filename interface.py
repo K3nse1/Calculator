bdb001b31c5b5e8d8ffd86d1b60cb68f45880f5a
K3nse1 <raulsangut999@gmail.com>
2023-10-12 15:06:38 +0200

    First version

--- interface.py
+++ interface.py
@@ -1,79 +1,90 @@
 import tkinter as tk
 import operaciones
 
-def interface ():
+def click (display, text):
+    display.delete (0)
+    display.insert (0, text)
 
-    root = tk.Tk()
+
+# def interface ():
+
+root = tk.Tk()
 
     # Tamaño de la ventana
-    root.geometry("500x400")
+root.geometry("500x400")
 
     # Título
-    root.title("Calculadora")
+root.title("Calculadora")
 
     # Icono
-    root.iconbitmap("images/icon.ico")
+root.iconbitmap("images/icon.ico")
 
     # Color del fondo
-    root.config(bg = "black")
+root.config(bg = "black")
 
     # Definimos nuestro frame
-    myFrame = tk.Frame()
+myFrame = tk.Frame()
 
     # Empaquetamos el frame en la raíz
-    myFrame.pack(fill = "x")
+myFrame.pack(fill = "x")
 
     # Damos color al frame para verlo
-    myFrame.config (bg = "white")
+myFrame.config (bg = "white")
 
-    myFrame.config(width = "650", height = "350")
+myFrame.config(width = "650", height = "350")
 
-    bt1 = tk.Button(myFrame, text="1", padx=2, pady=2)
-    bt1.grid (row=1, column=0)
+    # Creamos el display que mostrará los números
+display = tk.Entry (myFrame)
+display.grid (row=0, column=0, columnspan=4)
 
-    bt2 = tk.Button(myFrame, text="2", padx=2, pady=2)
-    bt2.grid(row=1, column=1)
+bt1 = tk.Button(myFrame, text="1", command = lambda: click(display, "1"), padx=2, pady=2)
+bt1.grid (row=1, column=0)
 
-    bt3 = tk.Button(myFrame, text="3", padx=2, pady=2)
-    bt3.grid(row=1, column=2)
+bt2 = tk.Button(myFrame, text="2", command = lambda: click(display, "2"), padx=2, pady=2)
+bt2.grid(row=1, column=1)
 
-    bt4 = tk.Button(myFrame, text="4", padx=2, pady=2)
-    bt4.grid(row=2, column=0)
+bt3 = tk.Button(myFrame, text="3", command = lambda: click(display, "3"), padx=2, pady=2)
+bt3.grid(row=1, column=2)
 
-    bt5 = tk.Button(myFrame, text="5", padx=2, pady=2)
-    bt5.grid(row=2, column=1)
+bt4 = tk.Button(myFrame, text="4", command = lambda: click(display, "4"), padx=2, pady=2)
+bt4.grid(row=2, column=0)
 
-    bt6 = tk.Button(myFrame, text="6", padx=2, pady=2)
-    bt6.grid(row=2, column=2)
+bt5 = tk.Button(myFrame, text="5", command = lambda: click(display, "5"), padx=2, pady=2)
+bt5.grid(row=2, column=1)
 
-    bt7 = tk.Button(myFrame, text="7", padx=2, pady=2)
-    bt7.grid(row=2, column=0)
+bt6 = tk.Button(myFrame, text="6", command = lambda: click(display, "6"), padx=2, pady=2)
+bt6.grid(row=2, column=2)
 
-    bt8 = tk.Button(myFrame, text="8", padx=2, pady=2)
-    bt8.grid(row=2, column=1)
+bt7 = tk.Button(myFrame, text="7", command = lambda: click(display, "7"), padx=2, pady=2)
+bt7.grid(row=2, column=0)
 
-    bt9 = tk.Button(myFrame, text="9", padx=2, pady=2)
-    bt9.grid(row=2, column=2)
+bt8 = tk.Button(myFrame, text="8", command = lambda: click(display, "8"), padx=2, pady=2)
+bt8.grid(row=2, column=1)
 
-    bt0 = tk.Button(myFrame, text="0", padx=2, pady=2)
-    bt0.grid(row=3, column=0)
+bt9 = tk.Button(myFrame, text="9", command = lambda: click(display, "9"), padx=2, pady=2)
+bt9.grid(row=2, column=2)
 
-    bt_dot = tk.Button(myFrame, text=".", command=operaciones.suma, padx=2, pady=2)
-    bt_dot.grid(row=3, column=1)
+bt0 = tk.Button(myFrame, text="0", command = lambda: click(display, "0"), padx=2, pady=2)
+bt0.grid(row=3, column=0)
 
-    bt_sum = tk.Button(myFrame, text="+", padx=2, pady=2)
-    bt_sum.grid(row=1, column=3)
+bt_dot = tk.Button(myFrame, text=".", command = lambda: click(display, "."), padx=2, pady=2)
+bt_dot.grid(row=3, column=1)
 
-    bt_dif = tk.Button(myFrame, text="-", padx=2, pady=2)
-    bt_dif.grid(row=2, column=3)
+bt_sum = tk.Button(myFrame, text="+", command=lambda: display.get() + display.get(), padx=2, pady=2)
+bt_sum.grid(row=1, column=3)
 
-    bt_mul = tk.Button(myFrame, text="x", padx=2, pady=2)
-    bt_mul.grid(row=3, column=3)
+bt_dif = tk.Button(myFrame, text="-", command=operaciones.resta, padx=2, pady=2)
+bt_dif.grid(row=2, column=3)
 
-    bt_div = tk.Button(myFrame, text="/", padx=2, pady=2)
-    bt_div.grid(row=3, column=2)
+bt_mul = tk.Button(myFrame, text="x", command=operaciones.multiplicacion, padx=2, pady=2)
+bt_mul.grid(row=3, column=3)
 
-    return root.mainloop()
+bt_div = tk.Button(myFrame, text="/", command=operaciones.division, padx=2, pady=2)
+bt_div.grid(row=3, column=2)
+
+root.mainloop()
+
+    # return root.mainloop()
 
 
-interface()
+# interface()
--- operaciones.py
+++ operaciones.py
@@ -1,2 +1,60 @@
+
 def suma (n1, n2):
     return n1 + n2
+
+
+def resta (n1, n2):
+    return n1 - n2
+
+
+def multiplicacion (n1, n2):
+    return n1 * n2
+
+
+def division (n1, n2):
+    return n1 / n2
+
+# import tkinter as tk
+#
+# # Función para actualizar el display
+# def actualizar_display(valor):
+#     display.config(text=valor)
+#
+# # Función para manejar los clics de los botones numéricos
+# def boton_numero_clicado(numero):
+#     valor_actual = display.cget("text")
+#     nuevo_valor = valor_actual + numero
+#     actualizar_display(nuevo_valor)
+#
+# # Función para manejar el clic del botón "C" (borrar)
+# def boton_borrar_clicado():
+#     actualizar_display("")
+#
+# # Configuración de la ventana principal
+# ventana = tk.Tk()
+# ventana.title("Calculadora")
+#
+# # Crear el display
+# display = tk.Label(ventana, text="", font=("Helvetica", 20))
+# display.grid(row=0, column=0, columnspan=4)
+#
+# # Crear los botones numéricos
+# botones_numeros = [
+#     "7", "8", "9",
+#     "4", "5", "6",
+#     "1", "2", "3",
+#     "0"
+# ]
+#
+# fila, columna = 1, 0
+# for numero in botones_numeros:
+#     tk.Button(ventana, text=numero, font=("Helvetica", 16), command=lambda num=numero: boton_numero_clicado(num)).grid(row=fila, column=columna)
+#     columna += 1
+#     if columna > 2:
+#         columna = 0
+#         fila += 1
+#
+# # Botón de borrar
+# tk.Button(ventana, text="C", font=("Helvetica", 16), command=boton_borrar_clicado).grid(row=5, column=0)
+#
+# ventana.mainloop()
